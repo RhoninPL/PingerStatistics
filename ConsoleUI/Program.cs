49e2a7274744fb7734631ef856952329960ed5e2
Michał Świtalik <michal.switalik@gmail.com>
2017-05-12 21:28:54 +0200

    Added library, moved methods. Some fixes.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PingerLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
@@ -8,50 +9,19 @@ namespace ConsoleUI
 {
     class Program
     {
-        public static List<long> pingTimes = new List<long>();
-        public static DateTime startTime;
-        public static bool sent;
+        
 
         static void Main(string[] args)
         {
-            startTime = DateTime.Now;
-            Timer timer = new Timer(5000);
-            timer.Elapsed += async (sender, e) => SendPingAsync();
-            timer.Start();
+            Console.WriteLine("Enter address to ping: (default is www.google.pl)");
+            var address = Console.ReadLine();
 
-            Timer timer2 = new Timer(3000);
-            timer2.Elapsed += async (sender, e) => StartDisplayInfo();
-            timer2.Start();
+            var pinger = new PingerLibrary.PingerLibrary(address);
+            pinger.Run();
 
             Console.ReadKey();
         }
 
-        public static void StartDisplayInfo()
-        {
-            if (sent)
-                Console.WriteLine($"Avg: {Math.Round(pingTimes.Average(), 2)}, Time: {(DateTime.Now - startTime).ToString("mm\\:ss")}");
-        }
-
-        public static async void SendPingAsync()
-        {
-            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-            byte[] buffer = Encoding.ASCII.GetBytes(data);
-            String url = "www.google.pl";
-            Ping ping = new Ping();
-            ping.PingCompleted += new PingCompletedEventHandler(DisplayReply);
-            await ping.SendPingAsync(url, 2500, buffer, new PingOptions(64, true));
-
-        }
-
-        public static void DisplayReply(object sender, PingCompletedEventArgs e)
-        {
-            PingReply pingReply = e.Reply;
-            if (pingReply.Status == IPStatus.Success)
-            {
-                pingTimes.Add(pingReply.RoundtripTime);
-                Console.WriteLine($"IP: {pingReply.Address}, Status: {pingReply.Status}, Time: {pingReply.RoundtripTime}");
-                sent = true;
-            }
-        }
+        
     }
 }
---
+++ PingerLibrary/PingerLibrary.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace PingerLibrary
+{
+    public class PingerLibrary
+    {
+        private string address;
+        public static List<long> pingTimes = new List<long>();
+        public static DateTime startTime;
+        public static bool sent;
+        public static int failed = 0;
+        private TimeSpan recordTime;
+
+        public PingerLibrary(string address = "www.google.pl", TimeSpan? recordTime = null)
+        {
+            this.address = address != "" ? address : "www.google.pl";
+            recordTime = recordTime ?? TimeSpan.FromMinutes(15);
+        }
+
+        public void Run()
+        {
+            startTime = DateTime.Now;
+            Timer timer = new Timer(5000);
+            timer.Elapsed += async (sender, e) => SendPingAsync(address);
+            timer.Start();
+
+            Timer timer2 = new Timer(3000);
+            timer2.Elapsed += async (sender, e) => StartDisplayInfo();
+            timer2.Start();
+        }
+
+        public static void StartDisplayInfo()
+        {
+            if (sent)
+                Console.WriteLine($"Avg: {Math.Round(pingTimes.Average(), 2)} ms, Time: {(DateTime.Now - startTime).ToString("mm\\:ss")} minutes. Lost: {failed}");
+        }
+
+        public static async void SendPingAsync(string address)
+        {
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            Ping ping = new Ping();
+            ping.PingCompleted += new PingCompletedEventHandler(DisplayReply);
+            await ping.SendPingAsync(address, 2500, buffer, new PingOptions(64, true));
+
+        }
+
+        public static void DisplayReply(object sender, PingCompletedEventArgs e)
+        {
+            PingReply pingReply = e.Reply;
+            if (pingReply.Status == IPStatus.Success)
+            {
+                pingTimes.Add(pingReply.RoundtripTime);
+                sent = true;
+            }
+            else
+                failed++;
+        }
+    }
+}
